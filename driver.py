6aa83e56ec1639a26d05628215290a4529e26f85
Vennela786 <vennela.malempati@slu.edu>
2023-03-03 00:10:52 -0600

    Training new datasets

--- driver.py
+++ driver.py
@@ -2,18 +2,25 @@ import cv2 as cv
 import numpy as np
 import pathlib 
 import train_model
+import configparser
+import predict
 
 training_dataset_path = pathlib.Path("/Users/moulis_mac/Desktop/AI/dataset")
 model_exportpath = "face_recognizer.cv2"
-
+img = '/Users/moulis_mac/Downloads/kane_(103).jpg'
+recognizer = cv.face.LBPHFaceRecognizer_create()
 # To load trained recognizer, if recognizer is not found it will train on the dataset images
 try:
-    recognizer = cv.face.LBPHFaceRecognizer_create()
     recognizer.read(model_exportpath)
-    print("Recognizer loaded")
+    print("Recognizer Loaded")
 except Exception as e:
     print("Trained model not found, training on dataset images...")
-    train_model.train_dataset_images(training_dataset_path, model_exportpath)
+    recognizer = train_model.train_dataset_images(training_dataset_path, model_exportpath)
     print("Model trained successfully")
+
+predict.predict_person(recognizer, img)
+
+
+
 
 
