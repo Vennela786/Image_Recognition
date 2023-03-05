77d280822888bf595ebc7af473613b5fbae78ab4
Vennela786 <vennela.malempati@slu.edu>
2023-03-04 18:14:23 -0600

    addigng properties

--- driver.py
+++ driver.py
@@ -4,20 +4,27 @@ import pathlib
 import train_model
 import configparser
 import predict
+import os
 
-training_dataset_path = pathlib.Path("C:\Users\venne\Image_Recognition\dataset")
-model_exportpath = "face_recognizer.cv2"
-img = 'kane_(103).jpg'
+config = configparser.RawConfigParser()
+config.read('properties.properties')
+
+training_dataset_path = pathlib.Path(config.get('FILES_PATH', 'training_dataset_path'))
+trained_recogniser = config.get('FILES_PATH', 'trained_recognizer_export')
+img = config.get('FILES_PATH', 'test_image')
 recognizer = cv.face.LBPHFaceRecognizer_create()
-# To load trained recognizer, if recognizer is not found it will train on the dataset images
-try:
-    recognizer.read(model_exportpath)
-    print("Recognizer Loaded")
-except Exception as e:
-    print("Trained model not found, training on dataset images...")
-    recognizer = train_model.train_dataset_images(training_dataset_path, model_exportpath)
-    print("Model trained successfully")
 
+# To load trained recognizer, if recognizer is not found it will train on the dataset images
+if os.path.exists(trained_recogniser):
+    recognizer.read(trained_recogniser)
+    print("Recognizer Loaded...")
+else:
+    print("Trained model not found, training on dataset images...")
+    recognizer = train_model.train_dataset_images(training_dataset_path, trained_recogniser)
+    print("Model trained successfully...")
 predict.predict_person(recognizer, img)
+
+
+
 
 
